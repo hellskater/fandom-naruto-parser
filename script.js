97345f5a4ab567b41d474bdea93dfa349dfdd970
hellskater <ksrinivasrao531@gmail.com>
2023-02-10 01:43:24 +0530

    formatted data

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("characters.json");
-const characters = JSON.parse(data);
+const clans = JSON.parse(fs.readFileSync("./data/villages.json"));
 
-// map over all the characters and give an array of unique team names sorted alphabetically
+// give each clan an id with id property on top
 
-const teams = characters
-  .map((character) => character?.personal?.kekkeiGenkai)
-  .filter((team) => team)
-  .flat()
-  .filter((team, index, self) => self.indexOf(team) === index)
-  .sort();
-
-// map over the teams and create an object with the team name as the key and an array of characters who have the team in their personal.team as the value
-
-const teamsWithCharacters = teams.map((team) => {
-  const charactersWithTeam = characters.filter((character) =>
-    character?.personal?.kekkeiGenkai?.includes(team)
-  );
-  return { name: team, characters: charactersWithTeam };
+clans.forEach((clan, index) => {
+  clan.id = index;
 });
 
-// write the teamsWithCharacters object to a json file
+// move the id property to the top
 
-fs.writeFileSync("kekkeiGenkai.json", JSON.stringify(teamsWithCharacters));
+const sortedClans = clans.map((clan) => {
+  const keys = Object.keys(clan);
+  const sortedKeys = keys.sort();
+  const sortedClan = {};
+  sortedKeys.forEach((key) => {
+    if (key === "id") {
+      sortedClan[key] = clan[key];
+    }
+  });
+  sortedKeys.forEach((key) => {
+    if (key === "name") {
+      sortedClan[key] = clan[key];
+    }
+  });
+  sortedKeys.forEach((key) => {
+    sortedClan[key] = clan[key];
+  });
+  return sortedClan;
+});
+
+// write
+
+fs.writeFileSync("./data/villages.json", JSON.stringify(sortedClans));
